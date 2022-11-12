63e6754a5488ba5e3bd9964e47bd9d31edb5af3c
wallodya <78960605+wallodya@users.noreply.github.com>
2022-11-12 12:27:38 +0400

    Observer

--- creational/Factory.js
+++ creational/Factory.js
@@ -1,12 +1,12 @@
 // Creating classes for diffirent type of mobs
-class Zombie {
+export class Zombie {
     constructor(_name, _healthPoints, _damage, _difficulty) {
         this.name = _name
         this.health = _healthPoints * (1 + Math.round(_difficulty / 10))
         this.damage = _damage * (1 + Math.round(_difficulty / 10))
     }
 }
-class Skeleton {
+export class Skeleton {
     constructor(_name, _healthPoints, _damage, _difficulty) {
         this.name = _name
         this.health = _healthPoints * (1 + Math.round(_difficulty / 10))
@@ -41,13 +41,13 @@ class MobFactory {
 }
 // Now we don`t need to memorize and call mob classes individually
 // We can just spawn them using static spawn method from MobFactory class like so:
-const zombieJorge = MobFactory.spawn('zombie', 'Jorge', 10, 4, 1)
-const skeletonMichel = MobFactory.spawn('skeleton', 'Michel', 8, 5, 2)
-let spiders = []
-for (let i = 1; i < 5; i++) {
-    const spider = MobFactory.spawn('spider', 'Spider' + i, 7, 8, Math.round(Math.random() * 10))
-    spiders.push(spider)
-}
+// const zombieJorge = MobFactory.spawn('zombie', 'Jorge', 10, 4, 1)
+// const skeletonMichel = MobFactory.spawn('skeleton', 'Michel', 8, 5, 2)
+// let spiders = []
+// for (let i = 1; i < 5; i++) {
+//     const spider = MobFactory.spawn('spider', 'Spider' + i, 7, 8, Math.round(Math.random() * 10))
+//     spiders.push(spider)
+// }
 
 // const mobs = [zombieJorge, skeletonMichel, ...spiders]
 // console.table(mobs)
